034704d735adadda7645c57e6d5bda47f985ccd7
kauanzin222 <kauaanthony.cardoso44@gmail.com>
2026-08-11 14:58:46 -0300

    Mudando o nome do jogo para português

--- SecretWord/src/components/TelaInicial.jsx
+++ SecretWord/src/components/TelaInicial.jsx
@@ -4,7 +4,7 @@ import styles from './TelaInicial.module.css'
 const TelaInicial = ({ jogar }) => {
     return (
         <div className='container'>
-            <h1>Secret Word</h1>
+            <h1>Palavra Secreta</h1>
             <p className={styles.subtitle}>Clique no botão abaixo para começar a jogar</p>
             <button onClick={jogar}>Começar jogo</button>
         </div>

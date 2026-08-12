7b5537e1254a5d2f720ff7f8e3f23fc3a043ede6
kauanzin222 <kauaanthony.cardoso44@gmail.com>
2026-08-12 16:43:16 -0300

    Remodulando estilo da página de endgame

--- SecretWord/src/components/FimDeJogo.jsx
+++ SecretWord/src/components/FimDeJogo.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import styles from './FimDeJogo.module.css'
 
-const FimDeJogo = ({
-  tentarNovamente,
-  pontuacao
-}) => {
+const FimDeJogo = ({ tentarNovamente, pontuacao }) => {
   return (
-    <div className='container'>
-      <h1>Fim de Jogo!</h1>
-      <p>Sua pontuação final foi: <span>{pontuacao}</span></p>
-      <button onClick={tentarNovamente}>Tentar novamente</button>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Fim de Jogo!</h1>
+
+      <div className={styles.pontuacaoBadge}>
+        Sua pontuação final foi: <span className={styles.pontuacaoNumero}>{pontuacao}</span>
+      </div>
+
+      <button className={styles.btnTentar} onClick={tentarNovamente}>
+        Tentar novamente
+      </button>
     </div>
   )
 }
